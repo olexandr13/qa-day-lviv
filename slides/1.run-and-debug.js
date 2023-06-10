a53c66f9f24b785f883074b214af3f647c168ea7
Oleksandr Pelykh <olexandr13@gmail.com>
2023-06-10 10:51:36 +0300

    upd

--- slides/1.run-and-debug.js
+++ slides/1.run-and-debug.js
@@ -7,5 +7,6 @@ add(2, 3);
 add(3, 4);
 add(4, 5);
 add(5, 6);
+add(6, 7);
 
 console.log('>> done');
--- slides/3.data-inspection_logpoint.js
+++ slides/3.data-inspection_logpoint.js
@@ -4,17 +4,23 @@ let myName = 'John';
 myAge = 25;
 myName = 'Steve';
 
-const arr = [1,325,,6765,456,8,32,26,8,54,2534,6,47,7,3536];
+const arr = [1, 325, , 6765, 456, 8, 32, 26, 8, 54, 2534, 6, 47, 7, 3536];
 
 const person = {
   age: myAge,
   city: 'Lviv',
   name: myName,
   parents: {
-    mother: 'Ann',
+    mother: { 
+      name: 'Ann',
+      age: 45,
+      occupation: 'killer',
+    },
     father: 'Pen',
-  }
+  },
 };
+
+
 
 myAge = 30;
 myName = 'Anton';
